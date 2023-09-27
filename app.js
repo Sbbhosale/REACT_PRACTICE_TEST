6dac7ce32d39c532faeec9a3940659a2c7e7a480
Sbbhosale <sb.bhosaleshubham@gmail.com>
2023-09-27 20:16:31 +0530

    feat : nested react elements

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 
-const heading = React.createElement("h1" , {} ,"This is the heading from React");
+const heading = React.createElement("h1" , {} ,[React.createElement("h1" , {} ,"This is inside Title"),React.createElement("h1" , {} ,"This is also inside title")]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
